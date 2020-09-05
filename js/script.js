b24455d1393ecec466f7608a3dbd2310aa363d32
1MrHous1 <50341247+1MrHous1@users.noreply.github.com>
2020-09-05 18:31:04 +0200

    set computer move

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
-let playerMove = "papier";
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+}
+
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+const computerInput = a => Math.floor(Math.random()*3 +1);
+
+if(computerInput == '1'){
+    computerMove = 'kamień';
+  } else if (computerInput == '2'){
+      computerMove = 'papier';
+  }
+  
+  if(computerInput == '3'){
+     computerMove = 'nożyce';
+  }
