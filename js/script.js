be33100ab8acc9cfa5e40e8c0541e9cdc1ed0f87
1MrHous1 <50341247+1MrHous1@users.noreply.github.com>
2020-09-05 18:37:44 +0200

    Reading the player's movement

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
+let playerMove = "nieznany ruch";
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput == '2'){
-    playerMove = 'papier';
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nożyce";
 }
 
-if(playerInput == '3'){
-    playerMove = 'nożyce';
+printMessage("Twój ruch to: " + playerMove);
+
+const computerInput = () => Math.floor(Math.random() * 3 + 1);
+
+if (computerInput == "1") {
+  computerMove = "kamień";
+} else if (computerInput == "2") {
+  computerMove = "papier";
+} else if (computerInput == "3") {
+  computerMove = "nożyce";
 }
 
-printMessage('Twój ruch to: ' + playerMove);
 
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Gracz wpisał: " + playerInput);
 
-const computerInput = a => Math.floor(Math.random()*3 +1);
-
-if(computerInput == '1'){
-    computerMove = 'kamień';
-  } else if (computerInput == '2'){
-      computerMove = 'papier';
-  }
-  
-  if(computerInput == '3'){
-     computerMove = 'nożyce';
-  }
