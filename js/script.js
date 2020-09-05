bcb97ef7fc300ba8602bbb40f85eb3686eab42eb
1MrHous1 <50341247+1MrHous1@users.noreply.github.com>
2020-09-05 19:01:21 +0200

    Game result

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ if (playerInput == "1") {
 } else if (playerInput == "3") {
   playerMove = "nożyce";
 }
+else if (playerMove == "nieznany ruch") {
+    printMessage('błąd !');
+  }
 
 printMessage("Twój ruch to: " + playerMove);
 
@@ -25,3 +28,31 @@ if (computerInput == "1") {
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
 console.log("Gracz wpisał: " + playerInput);
 
+
+
+if (computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+
+
+  else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('przegrałeś :/');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('przegrałeś :/');
+  }
+  else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('przegrałeś :/');
+  }
+
+
+
+  else if (computerMove == playerMove){
+    printMessage('Remis!');
+  }
